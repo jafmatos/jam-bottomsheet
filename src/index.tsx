e82609b2d0a7e5ee1d119921f1a12d413a7a8eff
= <anselmojose297@gmail.com>
2026-05-03 02:46:53 -0300

    feat: found out that keyboard controller and worklets versions were causing the errors in the testing app and now broke backdrop visibility

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-export {
-  BottomSheet,
-  type BottomSheetProps,
-  type BottomSheetRef,
-} from './components/BottomSheet';
-export { BottomSheetProvider } from './components/BottomSheetProvider';
