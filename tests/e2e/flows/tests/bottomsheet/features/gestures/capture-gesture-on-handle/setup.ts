b3c3eea5b3ec36dba868553c88fdd872e69cb071
Jose Anselmo Ferreira Matos <20261ADSGR0121@labs.garanhuns.ifpe>
2026-05-12 19:59:27 -0300

    feat: add handle gesture tests

---
+++ tests/e2e/flows/tests/bottomsheet/features/gestures/capture-gesture-on-handle/setup.ts
@@ -0,0 +1,12 @@
+import { BottomSheetPropsBuilder } from "@e2e/utils/BottomSheetPropsBuilder";
+import { BottomSheetContentOptions } from "@e2e/utils/ContentOptions";
+import { makeDeepLinkWithBottomSheetProps } from "@e2e/utils/makeDeepLinkWithBottomSheetProps";
+
+const props = BottomSheetPropsBuilder.one().withExpandable(true).withSnapPointsCollapsed(400).withSnapPointsExpanded(600).build();
+
+export default {
+  url: makeDeepLinkWithBottomSheetProps({
+    ...props,
+    content: BottomSheetContentOptions.Empty,
+  }),
+};
